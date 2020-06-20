83cb4c6d288357a5a1b09662ec79409c6bec1ffa
Oran C <Oranbusiness@gmail.com>
2020-06-19 20:32:40 -0700

    [added] logger

--- server.js
+++ server.js
@@ -10,32 +10,54 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const logger = require("winston");
+//setup console colorizing text
+const winston = require("winston");
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+      ),
+    defaultMeta: { service: 'user-service' },
+    transports: [,
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  });
+
+
 
 bot.on("message", (server_message) => {
-  let { content, author, ...rest } = server_message;
-  let { username, id, lastMessageID, createdAt, joinedAt } = author;
+    let { content, author, ...rest } = server_message;
+    let { username, id, lastMessageID, createdAt, joinedAt } = author;
 
-  console.log(`${username}:> ${content}`);
+    console.log(`${username}:> ${content}`);
 
-  //loki responds
-  if (content.includes("/loki")) {
-    server_message.reply(
-      "You must be mistaken... I am not loki, you are loki!"
-    );
-  }
+    //loki responds
+    if (content.includes("/loki")) {
+        server_message.reply(
+            "You must be mistaken... I am not loki, you are loki!"
+        );
+    }
 });
 
 bot.on("ready", () => {
-  console.log("Loki bot server: has started.");
+    logger.info("Connected");
+    logger;
+    logger.info("Loki bot server: has started.");
 });
 
 let { API_TOKEN, ENVIRONMENT } = process.env;
 
 if (ENVIRONMENT === "production") {
-  bot.login(API_TOKEN); //For Online Deploy
+    bot.login(API_TOKEN); //For Online Deploy
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+      }));
 } else if (ENVIRONMENT === "dev") {
-  console.log("API token: " + process.env.API_TOKEN);
-  console.log("Env: " + ENVIRONMENT);
-  bot.login(API_TOKEN); //For l
+    logger.level = "debug";
+    console.log("API token: " + process.env.API_TOKEN);
+    console.log("Env: " + ENVIRONMENT);
+    bot.login(API_TOKEN); //For l
 }
