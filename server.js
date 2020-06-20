a45d576668e6b64a52b36236e958e2451ec31d11
Oran C <Oranbusiness@gmail.com>
2020-06-19 20:17:11 -0700

    [added] loki now responds

--- server.js
+++ server.js
@@ -5,21 +5,37 @@
 // 20200619
 
 //load api token from .env
-require("dotenv").config()
+require("dotenv").config();
 
-
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-// console.log("API token: " + process.env.API_TOKEN)
+const logger = require("winston");
 
-bot.on("message", raw_message => {
-    console.log("Raw Message: " + raw_message)
-})
+bot.on("message", (server_message) => {
+  let { content, author, ...rest } = server_message;
+  let { username, id, lastMessageID, createdAt, joinedAt } = author;
 
-bot.on("ready", ()=>{
-    console.log("Loki bot server: has started.")
-})
+  console.log(`${username}:> ${content}`);
 
+  //loki responds
+  if (content.includes("/loki")) {
+    server_message.reply(
+      "You must be mistaken... I am not loki, you are loki!"
+    );
+  }
+});
 
-bot.login(process.env.API_TOKEN); //For Online Deploy
+bot.on("ready", () => {
+  console.log("Loki bot server: has started.");
+});
+
+let { API_TOKEN, ENVIRONMENT } = process.env;
+
+if (ENVIRONMENT === "production") {
+  bot.login(API_TOKEN); //For Online Deploy
+} else if (ENVIRONMENT === "dev") {
+  console.log("API token: " + process.env.API_TOKEN);
+  console.log("Env: " + ENVIRONMENT);
+  bot.login(API_TOKEN); //For l
+}
