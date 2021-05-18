ef373857c35a1888325533c5ef28a5561a36df0e
Oran C <Oranbusiness@gmail.com>
2021-05-17 20:35:34 -0700

    [add] 20210517 MASS UPDATE FOR COMPUTER BACKUP Search TAG BEaNcn4ncZ

--- server.js
+++ server.js
@@ -13,20 +13,21 @@ const bot = new Discord.Client();
 //setup console colorizing text
 const winston = require("winston");
 
+let state = 0;
+
 const logger = winston.createLogger({
-    level: 'info',
+    level: "info",
     format: winston.format.combine(
         winston.format.colorize(),
         winston.format.json()
-      ),
-    defaultMeta: { service: 'user-service' },
-    transports: [,
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' })
-    ]
-  });
-
-
+    ),
+    defaultMeta: { service: "user-service" },
+    transports: [
+        ,
+        new winston.transports.File({ filename: "error.log", level: "error" }),
+        new winston.transports.File({ filename: "combined.log" }),
+    ],
+});
 
 bot.on("message", (server_message) => {
     let { content, author, ...rest } = server_message;
@@ -40,6 +41,21 @@ bot.on("message", (server_message) => {
             "You must be mistaken... I am not loki, you are loki!"
         );
     }
+
+    if (content.includes("/adventure")) {
+
+      command = content.substr()
+        switch (state) {
+            case 0:
+                server_message.reply("You are standing infront of Vault13");
+                break;
+            case 0:
+                server_message.reply("Door to Vault13 are now closed.");
+                  break;
+            default:
+                break;
+        }
+    }
 });
 
 bot.on("ready", () => {
@@ -52,9 +68,11 @@ let { API_TOKEN, ENVIRONMENT } = process.env;
 
 if (ENVIRONMENT === "production") {
     bot.login(API_TOKEN); //For Online Deploy
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-      }));
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        })
+    );
 } else if (ENVIRONMENT === "dev") {
     logger.level = "debug";
     console.log("API token: " + process.env.API_TOKEN);
